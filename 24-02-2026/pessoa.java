7417fdcf8069a46e1162a661ab65adf2e6f64415
Ilan <engs-ilanperez@camporeal.edu.br>
2026-02-24 19:54:09 -0300

    fazer tarefaAula4POO, objetos e classes

---
+++ 24-02-2026/pessoa.java
@@ -0,0 +1,9 @@
+class pessoa {
+    String nome;
+    int idade;
+
+
+    void caminhar() {
+        System.err.println("esta caminhando...");
+    }
+}
---
+++ 24-02-2026/teste.java
@@ -0,0 +1,12 @@
+public class teste {
+
+    public static void main(String[] args) {
+        pessoa pessoa1 = new pessoa();
+
+            pessoa1.idade = 19;
+            pessoa1.nome = "JEff";
+
+            pessoa1.caminhar();
+            System.out.println(pessoa1.idade);
+    }
+}
